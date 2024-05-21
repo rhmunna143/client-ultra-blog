45b9c36948217006fc6bd0a51dba74d33e2ede47
rhmunna143 <rhmunna19@gmail.com>
2024-05-21 11:35:30 +0600

    footer added

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import "./globals.css";
 import AllContextProvider from "@/context/AllContext";
 import { Toaster } from "react-hot-toast";
 import NavBar from "@/components/Shared/NavBar";
+import Footer from "@/components/Footer/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,10 +18,16 @@ export default function RootLayout({ children }) {
     <html lang="en" data-theme="light">
       <body className={inter.className}>
         <AllContextProvider>
-          
+
           <NavBar />
 
-          {children}
+          <main className="min-h-screen">
+
+            {children}
+
+          </main>
+
+          <Footer />
 
         </AllContextProvider>
 
